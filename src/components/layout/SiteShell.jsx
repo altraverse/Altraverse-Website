606b1560d0a31d7de1be15f17d4483fc80530bd6
omparkhi <omparkhi23@gmail.com>
2026-04-22 00:10:26 +0530

    fixed errro

--- src/components/layout/SiteShell.jsx
+++ src/components/layout/SiteShell.jsx
@@ -2,7 +2,7 @@ import UniverseScene from "../canvas/UniverseScene";
 
 export default function SiteShell({ children }) {
   return (
-    <div className={cx("site-shell", className)}>
+    <div className="site-shell">
       <div className="ambient ambient-one" />
       <div className="ambient ambient-two" />
       <div className="ambient ambient-three" />
